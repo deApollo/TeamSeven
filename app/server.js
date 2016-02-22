c4a1e357df021eb07f9d296e6ffcf0f70bbe725d
Ezra Dowd <ezra.dowd@gmail.com>
2016-02-22 13:13:01 -0500

    Made some fixes to get things to run

--- app/server.js
+++ app/server.js
@@ -1,20 +1,18 @@
 var serverPort = 1337;
 var mongoPort = 27017;
-var dbname = mydatabase;
-var express = require('./config/express');
+var dbname = 'teamseven';
+var express = require('../config/express');
 var app = express();
 var db = require('./db');
 
 db.connect('mongodb://localhost:' + mongoPort + '/' + dbname, function(err) {
   if (err) {
-    console.log('Unable to connect to Mongo.')
-    process.exit(1)
+    console.log('Unable to connect to Mongo.');
+    process.exit(1);
   } else {
-    app.listen(port, function() {
-      console.log('Listening on port 3000...')
+    app.listen(serverPort, function() {
+      console.log('Listening on port 1337...');
+	  module.exports = app;
     })
   }
 })
-
-module.exports = app;
-console.log('Server running at http://localhost:' + port);
