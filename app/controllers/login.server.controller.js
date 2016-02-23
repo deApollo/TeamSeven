6c0df96537cd0f4cb5d7bbdf77e3abf5f7b52a22
Ezra Dowd <ezra.dowd@gmail.com>
2016-02-23 13:48:56 -0500

    Created basic database demo

--- app/controllers/login.server.controller.js
+++ app/controllers/login.server.controller.js
@@ -1,7 +1,7 @@
 var db = require('../db');
 var passwordHash = require('password-hash');
 
-exports.register = function(req, res, next) {
+exports.register = function(req, res) {
     var user = req.body.username;
     var pword = req.body.password;
     db.get().collection('users').find( { "username" : user } ).count(function(err, count){
@@ -9,17 +9,19 @@ exports.register = function(req, res, next) {
             var pwordHash = passwordHash.generate(pword);
             db.get().collection('users').insertOne({"username" : user, "password" : pwordHash});
             req.session.loggedin = true;
+            req.session.username = user;
             req.session.message = "Successfully registered user: " + user;
+            res.redirect("/upage");
         }
         else {
             req.session.loggedin = false;
             req.session.message = "A user with username: " + user + " already exists!";
+            res.redirect("/");
         }
-        next();
     });
 };
 
-exports.login = function(req, res, next) {
+exports.login = function(req, res) {
     var user = req.body.username;
     var pword = req.body.password;
     var cursor = db.get().collection('users').find( { "username" : user } );
@@ -28,25 +30,37 @@ exports.login = function(req, res, next) {
             var pwordHash = items[0]["password"];
             if(passwordHash.verify(pword,pwordHash)){
                 req.session.loggedin = true;
+                req.session.username = user;
                 req.session.message = "Welcome " + user + ", you have been logged in!";
+                res.redirect("/upage");
             }else{
                 req.session.loggedin = false;
                 req.session.message = "Invalid username or password!";
+                res.redirect("/");
             }
         } else {
             req.session.loggedin = false;
             req.session.message = "Invalid username or password!";
+            res.redirect("/");
         }
-        next();
     });
 };
 
-exports.logout = function(req, res, next){
+exports.logout = function(req, res){
     if(req.session.loggedin = true){
+        req.session.username = "";
         req.session.loggedin = false;
         req.session.message = "You have been logged out!";
     } else {
         req.session.message = "You aren't even logged in!";
     }
-    next();
+    res.redirect("/");
+};
+
+exports.validate = function(req,res,next){
+    if(req.session.loggedin){
+        next();
+    } else {
+        res.redirect("/");
+    }
 };
