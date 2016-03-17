774707b4a16d0927d6ffb2ef470fc4ba7ec5451c
Ezra Dowd <ezra.dowd@gmail.com>
2016-03-17 15:45:23 -0400

    Added the schema.js file -This file contains the schema descriptors for our mongoose implementation

---
+++ app/schema.js
@@ -0,0 +1,35 @@
+mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    firstname : String,
+    lastname : String,
+    username : { type : String, unique : true, required : true },
+    password : {type : String, required : true }
+});
+
+UserSchema.methods.getWorkouts = function(callback){
+    return this.model('Workout').find({username : this.username},callback);
+}
+
+var User = module.exports = mongoose.model('User',UserSchema);
+
+var ExerciseSchema = new mongoose.Schema({
+    username : String,
+    excercisename : String,
+    excercisedesc : String
+});
+
+ExerciseSchema.index({username : 1, excercisename : 1}, {unique : true});
+
+var Exercise = module.exports = mongoose.model('Exercise', ExerciseSchema)
+
+var WorkoutSchema = new mongoose.Schema({
+    workoutname : String,
+    workoutdesc : String,
+    username : String,
+    exercises : [{type : Schema.Types.ObjectId, ref : "Exercise"}]
+});
+
+WorkoutSchema.index({workoutname : 1, username : 1}, {unique : true});
+
+var Workout = module.exports = mongoose.model('Workout', WorkoutSchema)
